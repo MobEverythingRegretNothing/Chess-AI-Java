53b97866f148daccc1561950597e2f91ec7ac9b4
Charles Rathe <crrathe@gmail.com>
2019-11-01 14:11:42 -0500

    the rook works as expected, need to consider what data structures I want to make less duplication passing things around

--- Chess-AI-Java-Model/src/main/java/chess/model/piece/Piece.java
+++ Chess-AI-Java-Model/src/main/java/chess/model/piece/Piece.java
@@ -1,5 +1,14 @@
-package chess.model;
+package chess.model.piece;
+
+import chess.model.GameState;
+import chess.model.Move;
+import chess.model.Position;
+
+import java.util.List;
 
 public interface Piece {
 
+    List<Move> getValidMoves(Position startPosition, GameState gameState);
+
+    boolean isValidMove(Position startPosition, Position endPosition);
 }
